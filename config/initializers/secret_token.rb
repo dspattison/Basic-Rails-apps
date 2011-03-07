0b06db5178057b7cc875fbee271a20bebff02342
Ubuntu <ubuntu@ip-10-117-66-31.ec2.internal>
2011-03-07 08:31:23 +0000

    initalized rails env

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicRailsApps::Application.config.secret_token = 'd6865478bc1923ad9edb453b34ed6f8a197f8393e62e3ea8430d8427fa0e7a48ddee21af10dc64be85c42b7f832ea68500fcef51e2685b508ee040e9f57d0fba'
